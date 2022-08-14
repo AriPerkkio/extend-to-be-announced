763670402df5888ff67937e2c738465f9b4904f1
Ari Perkkiö <ari.perkkio@gmail.com>
2022-08-14 17:02:16 +0000

    chore(dev-deps): update all dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-module.exports = {
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
     preset: 'ts-jest',
+    testEnvironment: 'jsdom',
     verbose: true,
     setupFilesAfterEnv: ['./test/jest.setup.ts'],
 };
+
+module.exports = config;
