425c25b234d034956e202091cc2ebb45d586375c
Ari Perkkiö <ari.perkkio@gmail.com>
2024-08-05 07:42:04 +0300

    chore: lint fix

--- jest.js
+++ jest.js
@@ -1,2 +1,3 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 // Work-around entrypoint for jest@27 and below, https://jestjs.io/blog/2022/04/25/jest-28#packagejson-exports
 module.exports = require('./dist/jest/index.js');
